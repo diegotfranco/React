fd274e9cba41f70e4fb6d09044d0f596f48c7d4a
Diego Franco <diegotfranco@gmail.com>
2023-01-15 14:31:26 -0400

    commit inicial

---
+++ jsx/src/App.js
@@ -0,0 +1,14 @@
+function App () {
+    return (
+        <div className="wrapper">
+        <textarea
+            readOnly
+            maxLength={3}
+            spellCheck
+            style={{ backgroundColor: 'gray'}}
+        />
+        </div>
+    );   
+}
+
+export default App;
